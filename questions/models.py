e8250e5a0d0e673f21ee026e8b935d3168eed536
AllieK10 <93651852+AllieK10@users.noreply.github.com>
2023-07-19 16:25:16 +0300

    models.py

---
+++ questions/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from main.models import Task
+
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=200)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
+
+    def get_answers(self):
+        return self.answer_set.all()
+
+
+class Answer(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"question: {self.question.text}, answer: {self.text}, correct: {self.correct}"
